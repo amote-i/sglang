e44dacaa32819a57ef1c0ce50fe27d1f0a1e2adb
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2025-10-31 11:45:48 +0800

    Update test_retract_decode.py

--- test/srt/test_retract_decode.py
+++ test/srt/test_retract_decode.py
@@ -1,9 +1,8 @@
-import time
+import os
 import unittest
 from types import SimpleNamespace
 
-from sglang.srt.environ import envs
-from sglang.srt.utils import kill_process_tree
+from sglang.srt.utils import is_npu, kill_process_tree
 from sglang.test.run_eval import run_eval
 from sglang.test.test_utils import (
     DEFAULT_MODEL_NAME_FOR_TEST,
@@ -17,12 +16,29 @@ from sglang.test.test_utils import (
 class TestRetractDecode(CustomTestCase):
     @classmethod
     def setUpClass(cls):
-        cls.model = DEFAULT_MODEL_NAME_FOR_TEST
+        os.environ["SGLANG_TEST_RETRACT"] = "1"
+
+        cls.model = (
+            "/root/.cache/modelscope/hub/models/AI-ModelScope/Llama-3.1-8B-Instruct"
+            if is_npu()
+            else DEFAULT_MODEL_NAME_FOR_TEST
+        )
         cls.base_url = DEFAULT_URL_FOR_TEST
-        with envs.SGLANG_TEST_RETRACT.override(True):
-            cls.process = popen_launch_server(
-                cls.model, cls.base_url, timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH
-            )
+        other_args = (
+            [
+                "--attention-backend",
+                "ascend",
+                "--disable-cuda-graph",
+            ]
+            if is_npu()
+            else []
+        )
+        cls.process = popen_launch_server(
+            cls.model,
+            cls.base_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=other_args,
+        )
 
     @classmethod
     def tearDownClass(cls):
@@ -39,42 +55,37 @@ class TestRetractDecode(CustomTestCase):
 
         metrics = run_eval(args)
         self.assertGreaterEqual(metrics["score"], 0.65)
-        time.sleep(1)  # wait for mem check
-
-        assert self.process.poll() is None, "Server crashed during test"
 
 
 class TestRetractDecodeChunkCache(CustomTestCase):
     @classmethod
     def setUpClass(cls):
-        cls.model = DEFAULT_MODEL_NAME_FOR_TEST
-        cls.base_url = DEFAULT_URL_FOR_TEST
-        with envs.SGLANG_TEST_RETRACT.override(True):
-            cls.process = popen_launch_server(
-                cls.model,
-                cls.base_url,
-                timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-                other_args=["--disable-radix-cache", "--chunked-prefill-size", 128],
-            )
+        os.environ["SGLANG_TEST_RETRACT"] = "1"
 
-    def test_mmlu(self):
-        args = SimpleNamespace(
-            base_url=self.base_url,
-            model=self.model,
-            eval_name="mmlu",
-            num_examples=64,
-            num_threads=32,
+        cls.model = (
+            "/root/.cache/modelscope/hub/models/AI-ModelScope/Llama-3.1-8B-Instruct"
+            if is_npu()
+            else DEFAULT_MODEL_NAME_FOR_TEST
         )
-
-        metrics = run_eval(args)
-        self.assertGreaterEqual(metrics["score"], 0.65)
-        time.sleep(1)  # wait for mem check
-
-        assert self.process.poll() is None, "Server crashed during test"
-
-    @classmethod
-    def tearDownClass(cls):
-        kill_process_tree(cls.process.pid)
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        other_args = (
+            [
+                "--disable-radix-cache",
+                "--chunked-prefill-size",
+                128,
+                "--attention-backend",
+                "ascend",
+                "--disable-cuda-graph",
+            ]
+            if is_npu()
+            else ["--disable-radix-cache", "--chunked-prefill-size", 128]
+        )
+        cls.process = popen_launch_server(
+            cls.model,
+            cls.base_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=other_args,
+        )
 
 
 if __name__ == "__main__":

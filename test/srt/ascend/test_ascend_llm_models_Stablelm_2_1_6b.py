39629a1b8216bf071f7e23651e6f0674ae2f50c5
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2025-10-31 11:32:41 +0800

    Create test_ascend_llm_models_Stablelm_2_1_6b.py

---
+++ test/srt/ascend/test_ascend_llm_models_Stablelm_2_1_6b.py
@@ -0,0 +1,6 @@
+from test_ascend_llm_models_2 import TestLlama_2_7B
+
+
+class TestStablelm_2_1_6b(TestLlama_2_7B):
+    model = "/root/.cache/modelscope/hub/models/Xenova/stablelm-2-1_6b"
+    accuracy = 0.6
